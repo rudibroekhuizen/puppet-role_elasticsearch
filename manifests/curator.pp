1dc5e657e57cdd6124f652d2c9babc4b6b1c233d
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-04 11:35:47 +0200

    Update curator.pp

--- manifests/curator.pp
+++ manifests/curator.pp
@@ -3,18 +3,21 @@
 class role_elasticsearch::curator {
 
   # Install curator
-  class { 'curator':
-    package_provider => pip,
+  package { curator:
+    provider => pip,
+  }
+
+  # Configure curator.yml file using https://github.com/cataphract/puppet-yaml_settings module
+  yaml_settings { '/.root/.curator/curator.yml':
+    values => $role_elasticsearch::curator_yml, 
   }
   
-  #package { curator:
-  #  provider => pip,
-  #}
+  # Configure actions.yml file using https://github.com/cataphract/puppet-yaml_settings module
+  yaml_settings { '/.root/.curator/actions.yml':
+    values => $role_elasticsearch::actions_yml,
+  }
   
-  # Create actions
-  create_resources('curator::action', $role_elasticsearch::curator_action_hash)
-
-  # Schedle curator
+  # Schedule curator
   cron { "curator_run":
     ensure  => 'present',
     command => '/usr/local/bin/curator --config /root/.curator/curator.yml /root/.curator/actions.yml >/dev/null',
@@ -22,19 +25,5 @@ class role_elasticsearch::curator {
     minute  => 10,
     weekday => '*',
   }
-
-
-
-# Configure .yml file using https://github.com/cataphract/puppet-yaml_settings module
-#  yaml_settings { '/.root/.curator/curator.yml':
-#    values => $role_elasticsearch::curator_yml, 
-#  }
-  
-#  yaml_settings { '/.root/.curator/actions.yml':
-#    values => $role_elasticsearch::actions_yml,
-#  }
-
   
 }
-
-# Wrapper for https://github.com/cristifalcas/puppet-curator
