5ce07889b663251a9f3f89f9e6556ed80805005d
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-04 17:05:29 +0200

    Update curator.pp

--- manifests/curator.pp
+++ manifests/curator.pp
@@ -8,7 +8,7 @@ class role_elasticsearch::curator {
   }
 
   # Configure curator.yml file using https://github.com/cataphract/puppet-yaml_settings module
-  #yaml_settings { '/root/.curator/curator.yml':
+  # yaml_settings { '/root/.curator/curator.yml':
   #  values => $role_elasticsearch::curator_yml, 
   #}
   
